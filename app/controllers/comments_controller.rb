61d72fe941d51ad6aff3fd616e64bdbc6f617f0d
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-11-30 09:37:01 -0500

    create comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,15 @@
 class CommentsController < ApplicationController
 
     def create
-        # binding.pry
+         @article = Article.find(params[:article_id])
+         @comment = @article.comments.create(comment_params)
+         redirect_to article_path(@article)
     end
 
-  def new
-  end
+    private
+        def comment_params
+            params.require(:comment).permit(:commenter, :body)
+        end
 
-  def edit
-  end
 
 end
